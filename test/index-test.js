073b434741ad8a745bbbcab5e4fda988542fe655
Dev_Ricky <eric.kiogora12@gmail.com>
2022-06-04 19:51:49 +0300

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,20 +1,16 @@
 const { name, height, message } = require("../index.js");
 
-/*
 describe('what-is-a-test', () => {
   describe('Name', () => {
     it('returns "Susan"', () => {
       expect(name).toEqual('Susan')
     })
   })
-
-
   describe('Height', () => {
     it('is less than 40', () => {
       expect(height).toBeLessThan(40)
     })
   })
-
   describe('Message', () => {
     it('gives the name and height', () => {
       expect(message).toInclude(name)
@@ -22,4 +18,3 @@ describe('what-is-a-test', () => {
     })
   })
 })
-*/
